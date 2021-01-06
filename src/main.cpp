f11864fb0420e8478fde2dd34c88bba301827662
gustavo <gustavo_cunhaavila@hotmail.com>
2021-01-05 22:54:28 -0300

    Vertex Arrays in OpenGL

--- src/main.cpp
+++ src/main.cpp
@@ -103,8 +103,12 @@ int main() {
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -125,10 +129,10 @@ int main() {
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     float positions[] = {
-            -0.5f, -0.5f, // 0
-            0.5f, -0.5f, // 1
-            0.5f, 0.5f, // 2
-            -0.5f, 0.5f, // 3
+            -0.5f, -0.5f,   // 0
+            0.5f, -0.5f,   // 1
+            0.5f, 0.5f,   // 2
+            -0.5f, 0.5f,   // 3
     };
 
     unsigned int indices[] = {
@@ -136,49 +140,60 @@ int main() {
             2, 3, 0,
     };
 
-    unsigned int buffer;
-    GLCall(glGenBuffers(1, &buffer));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-    GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao))
+    GLCall(glBindVertexArray(vao))
 
-    GLCall(glEnableVertexAttribArray(0));
-    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+    unsigned int buffer;
+    GLCall(glGenBuffers(1, &buffer))
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer))
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW))
+
+    GLCall(glEnableVertexAttribArray(0))
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0))
 
     unsigned int ibo;
-    GLCall(glGenBuffers(1, &ibo));
-    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
+    GLCall(glGenBuffers(1, &ibo))
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo))
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW))
 
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     unsigned int shaders = CreateShader(source.VertexSource, source.FragmentSource);
-    GLCall(glUseProgram(shaders));
+    GLCall(glUseProgram(shaders))
 
     int location = glGetUniformLocation(shaders, ("u_Color"));
-    ASSERT(location != -1);
+    ASSERT(location != -1)
 
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindVertexArray(0))
+    GLCall(glUseProgram(0))
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0))
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0))
 
     float r = 0.0f;
     float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
-        GLCall(glClear(GL_COLOR_BUFFER_BIT));
+        GLCall(glClear(GL_COLOR_BUFFER_BIT))
 
-        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+        GLCall(glUseProgram(shaders))
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f))
+        GLCall(glBindVertexArray(vao))
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo))
+
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr))
 
         if (r > 1.0f) increment = -0.05f;
         else if (r < 0.0f) increment = 0.05f;
         r += increment;
         /* Swap front and back buffers */
-        GLCall(glfwSwapBuffers(window));
+        GLCall(glfwSwapBuffers(window))
 
         /* Poll for and process events */
-        GLCall(glfwPollEvents());
+        GLCall(glfwPollEvents())
     }
 
-    GLCall(glDeleteProgram(shaders));
+    GLCall(glDeleteProgram(shaders))
 
     glfwTerminate();
     return 0;
